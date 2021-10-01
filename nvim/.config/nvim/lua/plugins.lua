addc974c4795bd2c84d283d3f20cc0e9995937d7
orelian <orelian.kohler@epfl.ch>
2021-10-01 11:27:18 +0200

    Adding new neovim plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -4,6 +4,7 @@ return require('packer').startup(function()
 	use 'tpope/vim-commentary'
 	use 'tpope/vim-repeat'
 	use 'tpope/vim-sensible'
+	use 'tpope/vim-fugitive'
 	use {
 		'junegunn/fzf.vim',
 		requires = {
@@ -30,4 +31,8 @@ return require('packer').startup(function()
 	}
 
 	use 'nvim-treesitter/nvim-treesitter'
+	use 'suoto/vim-hdl'
+	use 'vim-syntastic/syntastic'
+	use({'scalameta/nvim-metals'})
+  	use 'mhinz/vim-signify'	
 end)
